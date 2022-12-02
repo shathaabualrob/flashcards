05c317c036d0ecda0339d843763302ab080e676e
Shatha AR <shatharub5@gmail.com>
2022-12-02 23:21:31 +0200

    Initial commit

---
+++ cards/urls.py
@@ -0,0 +1,26 @@
+from . import views
+from django.urls import path
+
+# from django.views.generic import TemplateView
+urlpatterns = [
+    # path(
+    #     "",
+    #     TemplateView.as_view(template_name="cards/base.html"),
+    #     name="home"
+    # ),
+    path(
+        "",
+        views.CardListView.as_view(),
+        name="card-list"
+        ),
+    path(
+        "new",
+        views.CardCreateView.as_view(),
+        name="card-create"
+        ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+        ),
+]
